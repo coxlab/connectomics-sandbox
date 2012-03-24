7c41c0f788c9e7670f0a964331797d3faad287ac
poilvert <nicolas.poilvert@gmail.com>
2012-03-24 17:56:11 -0400

    FIX: switch val and tst in cross validation generator

--- connectomics/trn_tst_val_generator.py
+++ connectomics/trn_tst_val_generator.py
@@ -17,9 +17,9 @@ from skimage.shape import view_as_blocks
 # -- default dtype for all images and annotations
 DTYPE = np.float32
 
-# -- default image indices for train/test and val sets
-DEFAULT_TRN_TST_IMG_Z_IDX = [71]
-DEFAULT_VAL_IMG_Z_IDX = [3]
+# -- default image indices for train/val and test sets
+DEFAULT_TRN_VAL_IMG_Z_IDX = [71]
+DEFAULT_TST_IMG_Z_IDX = [3]
 
 # -- whether we use normalized images (zero mean, unit variance)
 NORMALIZE = True
@@ -30,8 +30,8 @@ EPSILON = 0.2
 
 
 def generate(divide_factor=2,
-             trn_tst_img_z_idx=DEFAULT_TRN_TST_IMG_Z_IDX,
-             val_img_z_idx=DEFAULT_VAL_IMG_Z_IDX
+             trn_val_img_z_idx=DEFAULT_TRN_VAL_IMG_Z_IDX,
+             tst_img_z_idx=DEFAULT_TST_IMG_Z_IDX
              ):
 
     # -----------------------
@@ -46,26 +46,26 @@ def generate(divide_factor=2,
     # -- Extract raw images and annotations
     # -------------------------------------
 
-    # -- select trn/tst and val images
-    trn_tst_raw_imgs = [imread(imgd['filename'], flatten=True).astype(DTYPE)
+    # -- select trn/val and tst images
+    trn_val_raw_imgs = [imread(imgd['filename'], flatten=True).astype(DTYPE)
                         for imgd in annotated_metadata if imgd['z'] in
-                        trn_tst_img_z_idx
+                        trn_val_img_z_idx
                         ]
-    val_raw_imgs = [imread(imgd['filename'], flatten=True).astype(DTYPE)
+    tst_raw_imgs = [imread(imgd['filename'], flatten=True).astype(DTYPE)
                     for imgd in annotated_metadata if imgd['z'] in
-                    val_img_z_idx
+                    tst_img_z_idx
                     ]
 
     # -- corresponding Ground Truth annotations
-    trn_tst_gt_raw_imgs = \
+    trn_val_gt_raw_imgs = \
             [obj.get_annotation(imgd['annotation'][0]).astype(DTYPE)
                 for imgd in annotated_metadata if imgd['z'] in
-                trn_tst_img_z_idx
+                trn_val_img_z_idx
                 ]
-    val_gt_raw_imgs = \
+    tst_gt_raw_imgs = \
             [obj.get_annotation(imgd['annotation'][0]).astype(DTYPE)
                 for imgd in annotated_metadata if imgd['z'] in
-                val_img_z_idx
+                tst_img_z_idx
                 ]
 
     # ----------------------------------------------------
@@ -75,42 +75,42 @@ def generate(divide_factor=2,
 
     # -- we may also want to normalize the images
     if NORMALIZE:
-        trn_tst_imgs = []
-        for img in trn_tst_raw_imgs:
+        trn_val_imgs = []
+        for img in trn_val_raw_imgs:
             new_img = (img - img.mean()) / img.std()
-            trn_tst_imgs += [new_img]
-        val_imgs = []
-        for img in val_raw_imgs:
+            trn_val_imgs += [new_img]
+        tst_imgs = []
+        for img in tst_raw_imgs:
             new_img = (img - img.mean()) / img.std()
-            val_imgs += [new_img]
+            tst_imgs += [new_img]
     else:
-        trn_tst_imgs = trn_tst_raw_imgs
-        val_imgs = val_raw_imgs
+        trn_val_imgs = trn_val_raw_imgs
+        tst_imgs = tst_raw_imgs
 
     # -- we want annotations to be {-1., +1.} with a slight
     #    Gaussian smear (to help a little the classifier)
-    trn_tst_gt_imgs = []
-    for img in trn_tst_gt_raw_imgs:
+    trn_val_gt_imgs = []
+    for img in trn_val_gt_raw_imgs:
         new_img = np.where(img <= 0., -1., 1.)
         new_img = gaussian_filter(new_img, SIGMA)
         new_img = np.where(new_img > -1. + EPSILON, 1., -1.)
-        trn_tst_gt_imgs += [new_img]
-    val_gt_imgs = []
-    for img in val_gt_raw_imgs:
+        trn_val_gt_imgs += [new_img]
+    tst_gt_imgs = []
+    for img in tst_gt_raw_imgs:
         new_img = np.where(img <= 0., -1., 1.)
         new_img = gaussian_filter(new_img, SIGMA)
         new_img = np.where(new_img > -1. + EPSILON, 1., -1.)
-        val_gt_imgs += [new_img]
+        tst_gt_imgs += [new_img]
 
-    assert len(trn_tst_imgs) == len(trn_tst_gt_imgs)
-    assert len(val_imgs) == len(val_gt_imgs)
+    assert len(trn_val_imgs) == len(trn_val_gt_imgs)
+    assert len(tst_imgs) == len(tst_gt_imgs)
 
     # -------------------------------------
     # -- Creating the cross validation sets
     # -------------------------------------
 
     # -- images original size
-    h, w = trn_tst_imgs[0].shape[:2]
+    h, w = trn_val_imgs[0].shape[:2]
 
     # -- size of final images after we split the original
     #    images
@@ -122,46 +122,46 @@ def generate(divide_factor=2,
     h_tot = int(divide_factor) * h_new
     w_tot = int(divide_factor) * w_new
 
-    # -- loop over cross validation folds to build the train/test set
-    trn_tst_ll = []
+    # -- loop over cross validation folds to build the train/val set
+    trn_val_ll = []
     for i in xrange(int(divide_factor)):
         for j in xrange(int(divide_factor)):
 
-            # -- list to contain all the training/testing and gt
+            # -- list to contain all the training/validation and gt
             #    images for the specified cross validation fold
             trn_l = []
-            tst_l = []
+            val_l = []
             trn_gt_l = []
-            tst_gt_l = []
+            val_gt_l = []
 
-            # -- list of training/testing images
-            for img, gt in zip(trn_tst_imgs, trn_tst_gt_imgs):
+            # -- list of training/validation images
+            for img, gt in zip(trn_val_imgs, trn_val_gt_imgs):
                 blk_view = view_as_blocks(img[:h_tot, :w_tot], (h_new, w_new))
                 blk_view_gt = view_as_blocks(gt[:h_tot, :w_tot], (h_new, w_new))
-                tst_l += [blk_view[i, j, :, :]]
-                tst_gt_l += [blk_view_gt[i, j, :, :]]
+                val_l += [blk_view[i, j, :, :]]
+                val_gt_l += [blk_view_gt[i, j, :, :]]
                 for l in xrange(int(divide_factor)):
                     for p in xrange(int(divide_factor)):
                         if (l != i) or (p != j):
                             trn_l += [blk_view[l, p, :, :]]
-                            trn_gt_l = [blk_view_gt[l, p, :, :]]
+                            trn_gt_l += [blk_view_gt[l, p, :, :]]
 
-            # -- append new lists of train/test images to
-            #    ``trn_tst_ll``
-            trn_tst_ll.append([trn_l, trn_gt_l, tst_l, tst_gt_l])
+            # -- append new lists of train/val images to
+            #    ``trn_val_ll``
+            trn_val_ll.append([trn_l, trn_gt_l, val_l, val_gt_l])
 
-    # -- build the validation set
-    val_l = []
-    val_gt_l = []
-    for img, gt in zip(val_imgs, val_gt_imgs):
+    # -- build the testing set
+    tst_l = []
+    tst_gt_l = []
+    for img, gt in zip(tst_imgs, tst_gt_imgs):
         blk_view = view_as_blocks(img[:h_tot, :w_tot], (h_new, w_new))
         blk_view_gt = view_as_blocks(gt[:h_tot, :w_tot], (h_new, w_new))
         for i in xrange(int(divide_factor)):
             for j in xrange(int(divide_factor)):
-                val_l += [blk_view[i, j, :, :]]
-                val_gt_l += [blk_view_gt[i, j, :, :]]
-    val_ll = [val_l, val_gt_l]
+                tst_l += [blk_view[i, j, :, :]]
+                tst_gt_l += [blk_view_gt[i, j, :, :]]
+    tst_ll = [tst_l, tst_gt_l]
 
-    sets = dict(trn_tst=trn_tst_ll, val=val_ll)
+    sets = dict(trn_val=trn_val_ll, tst=tst_ll)
 
     return sets
